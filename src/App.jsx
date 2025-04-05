35ddb7f0ad6cabc6c21fa3b532055e54fb8b7924
Joe Concha Vasquez <joeconchavasquez@gmail.com>
2025-04-04 23:32:07 -0500

    Effects made

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 import WindowTracker from "./challenge-components/windows-tracker";
 import Header from "./components/header/header.component";
 import Main from "./components/main/main.component";
 
 function App() {
-  const [show, setShow] = useState(true);
-
-  const handleToglle = () => {
-    setShow((prevShow) => !prevShow);
-  };
   return (
     <>
       <Header />
       <Main />
-
-      <button onClick={handleToglle}>
-        {!show ? "Show" : "Hide"} WindowTracker
-      </button>
-      {show && <WindowTracker />}
     </>
   );
 }
--- src/challenge-components/windows-tracker.jsx
+++ src/challenge-components/windows-tracker.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function WindowTracker() {
-  return <h1>Window width: {window.innerWidth}</h1>;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const watchWindowSize = () => {
+      console.log("Window resized");
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", watchWindowSize);
+    return () => {
+      window.removeEventListener("resize", watchWindowSize);
+      console.log("Cleanup");
+    };
+  }, []);
+  return <h1>Window width: {windowWidth} px.</h1>;
 }
+
+/**
+ * 
+ * const [show, setShow] = useState(true);
+ 
+   const handleToglle = () => {
+     setShow((prevShow) => !prevShow);
+ * 
+ *  <button onClick={handleToglle}>
+        {!show ? "Show" : "Hide"} WindowTracker
+      </button>
+      {show && <WindowTracker />}
+ */
